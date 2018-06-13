07781c4237dfcf59b711bfe63ab7cb4682cc2aa7
Ziipp21 <38344531+Ziipp21@users.noreply.github.com>
2018-06-13 15:27:05 +0200

    oups

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -2,7 +2,7 @@
     <img src="/img/Logo-integralFoods.png" alt="logo" id="logo">
     Integral Foods Pro
     <a href="https://www.integralfoods.fr/" >Integral foods</a>
-    <a href="/articles">Produits</a>
+    <a href="/produits">Produits</a>
     <a href="">Mon compte</a>
 
     <a href="/panier">Mon panier</a>
