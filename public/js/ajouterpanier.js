d29761a93d3a56e879b139ce292863a8263ab922
Ziipp21 <38344531+Ziipp21@users.noreply.github.com>
2018-06-18 12:53:12 +0200

    modif produit

--- public/js/ajouterpanier.js
+++ public/js/ajouterpanier.js
@@ -1,8 +1,31 @@
-jQuery( "button" ).click(function() {
+jQuery( ".catalogueclient button" ).click(function() {
     let idArticle = $(this).parent().find('input').data("id");
     let quantite = $(this).parent().find('input').val();
     // console.log(quantite);
     jQuery.get("/panier/ajouter/"+idArticle+"x"+quantite);
-    $(this).parent().find('input').val('');
-    alert('ajouter au panier !');
+    $(this).parent().find('input').val(''); 
 })
+
+
+
+
+
+    $(".catalogueclient button").click(function () {
+        if ($("#qte").val().length > 0) {
+            $("#message")
+                .text("test reussi")
+                .fadeIn(3000, function () {
+                    $(this).fadeOut(3000, function () {
+                        sendMail();
+                    });
+                });
+        }
+        else {
+            $("#nom")
+                .next(".error-message")
+                .fadeIn(800)
+                .text("Entrer un nom");
+        }
+        return false;
+    });
+
--- resources/views/articlesclients/indexclient.blade.php
+++ resources/views/articlesclients/indexclient.blade.php
@@ -7,19 +7,19 @@
 <div class="d-flex justify-content-center flex-wrap">
 
 <!-- <div id="Recherher">
-  <input class="typeahead" type="text" placeholder="Rechercher" OnKeyPress="if (event.keyCode == 13)recherche()" >
+  <input  type="text" placeholder="Rechercher">
 </div>
 
 <input type = 'submit' value = 'Rechercher' OnClick="recherche()"> -->
 
 
 @foreach($articleclients as $articleclient)
-
-<article>
+<div id='message'></div>
+<article class="catalogueclient">
 
         <h3 class="nom">{{ $articleclient->nom }}</h3>
-            <p class="ref"><i>Réf : {{ $articleclient->reference }}</i></p>
-            <p class="ref"><i>EAN : {{ $articleclient->ean }}</i></p>
+            <p class="ref"><i>Réf : {{ $articleclient->reference }}</i></br>
+             <i>EAN : {{ $articleclient->ean }}</i></p>
     <details>
 
         <summary>Voir detail</summary>
@@ -30,7 +30,6 @@
     
         <img class="imgarticle" height="200" src="/storage/photo/{{ $articleclient->reference }}.png" class="" alt="" /><br>
     <div class="ajout">
-        <p>EAN : <span>{{ $articleclient->ean }}</span></p>
         <label id="qte">Quantité : <input class="quantite" placeholder="Qté" aria-labelledby="quantité de {{ $articleclient->nom }}"
             class="" step="1" min="0"  title="Qté" pattern="[0-9]*"
             type="number" inputmode="numeric" data-id="{{ $articleclient->catalogue_id }}"/></label></br>
