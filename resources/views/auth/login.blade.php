799cb4aac68bb7f6ba6964bf5b4a79eecab2b996
europrimus <europrimus@free.fr>
2018-06-20 14:40:43 +0200

    amélioration visuel

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -13,35 +13,43 @@
 
 <div class="container">
   <h1>Integral Foods Pro</h2>
-  <img src="/img/Logo-integralFoods.png" alt="logo integral food" >
+  <div class="logo">
+    <img src="/img/Logo-integralFoods.png" alt="logo integral food" >
+  </div>
 
   <h2>Identification</h2>
   <form method="POST" action="{{ action('CoController@check') }}" enctype="multipart/form-data">
     @csrf
 
-    <label for="id" class="">Identifiant</label>
-    <input id="id" type="id" name="id" value="{{ old('id') }}" required autofocus
+    <div class="input-group mb-3">
+      <div class="input-group-prepend">
+        <label for="id" class="input-group-text">Identifiant</label>
+      </div>
+      <input id="id" type="id" name="id" value="{{ old('id') }}" required autofocus
         class="{{ $errors->has('id') ? 'is-invalid' : '' }}">
-    @if ($errors->has('id'))
-      <span class="invalid">{{ $errors->first('id') }}</span>
-    @endif
-  <br>
+      @if ($errors->has('id'))
+        <span class="invalid">{{ $errors->first('id') }}</span>
+      @endif
+    </div>
 
-    <label for="password" class="">Mot de passe</label>
-    <input id="password" type="password" name="password" required
-      class="{{ $errors->has('password') ? 'is-invalid' : '' }}">
-    @if ($errors->has('password'))
-      <span class="invalid">{{ $errors->first('password') }}</span>
-    @endif
-  <br>
-
+    <div class="input-group mb-3">
+      <div class="input-group-prepend">
+        <label for="password" class="input-group-text">Mot de passe</label>
+      </div>
+      <input id="password" type="password" name="password" required
+        class="{{ $errors->has('password') ? 'is-invalid' : '' }}">
+      @if ($errors->has('password'))
+        <span class="invalid">{{ $errors->first('password') }}</span>
+      @endif
+    </div>
+<!--
     <label for="remember" class="">Se souvenir de moi</label>
       <input type="checkbox" id="remember" name="remember" {{ old('remember') ? 'checked' : '' }}>
   <br>
-
-    <button type="submit" class="">Se connecter</button>
+-->
+    <button type="submit" class="btn">Se connecter</button>
   <br>
-    <a class="" href="{{ route('password.request') }}">Mot de passe oublié</a>
+    <!-- <a class="" href="{{ route('password.request') }}">Mot de passe oublié</a> -->
   </form>
   <a href="/preinscription">Inscription</a>
 </div>
--- todo.md
+++ todo.md
@@ -1,8 +1,6 @@
 # todo
 ## Pre-inscription
 Amélioré le visuelle  
-gérer les erreurs  
-champ tel, code postale: vérifier nombre  
 commentaire vide  
 L'enregistrement dans la BDD ce fait même si il y a des erreurs dans l'adresse!  
 ## Connexion
@@ -11,24 +9,19 @@ tester mot de passe oublié
 Ajouter adresse facturation  
 Ajouter adresse livraison  
 ## Espace administration
-vérifier si admin  
-gestion du menu si admin ou non  
 ### Validation inscription
 envoyer le login et mot de passe  
 ### Commandes
-liste des commandes  
 valider payement  
 ## liste des produits
 ## Passer une commande
 vérifier panier: nombre articles supérieurs à 0  
-vérifier adresse livraison (ajouter message erreur)  
 ### enregistrement commande
 ### liste des commandes
 état de la commande  
 ### détails d'une commande
 ## Mon Compte
 ### Mes informations
-pouvoir modifier email, téléphone, nom du contacte
 ### Mes adresses
 pouvoir supprimer et ajouter des adresses de livraisons
 pouvoir modifier l'adresse de contact et de facturation
