6fa35a832dabe79882e8f545eaa08c574211aa49
europrimus <europrimus@free.fr>
2018-06-20 09:28:06 +0200

    compte admin par default

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,6 +12,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+      DB::table('utilisateur')->insert([
+        'civilite'=> 'Monsieur',  //['Monsieur','Madame']);
+        'nom' => 'Integral',
+        'prenom' => 'Food',
+        'entreprise' => 'Integral Food',
+        'etablissement'=> 'autre', //['distributeur','restaurant indépendant','chaîne de restaurants','épicerie','collectivité','traiteur','industrie','autre']
+        'tel' => '0972626718',
+        'email' => 'contact@integralfoods.fr',
+        'siret' => '799852934',
+        'kbis' => '',
+        'login' => 'admin',
+        'password' => password_hash( "secret" , PASSWORD_DEFAULT ),
+        'commentaire' => 'Administateur',
+        'role' => 'administrateur',
+      ]);
       $this->call([donneesFictives::class]);
     }
 }
